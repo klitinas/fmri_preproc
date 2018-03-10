59dcd5b03fc66b81c3cbf1fbb8cef7a1cd8ad9aa
fmril lab <fmrilab@toro.engin.umich.edu>
2018-03-09 21:37:26 -0500

    added more module functionality, including fm correction

--- scripts/func.py
+++ scripts/func.py
@@ -11,13 +11,13 @@ import shutil
 def run_module(RAWDATA,MODULE,PARAMS):
 
     CMD,OUTDATA = make_script_call(RAWDATA,MODULE,PARAMS)
-    #subprocess.call(CMD,shell=True)
+    subprocess.call(CMD,shell=True)
     print(CMD)
     return(OUTDATA)
 
 
 
-# Parse params and return script call string
+# Parse params and return script call string for all avail modules
 def make_script_call(INDATA,MODULE,PARAMS):
     OUTSTR=""
     OUTDATA=""
@@ -66,8 +66,19 @@ def make_script_call(INDATA,MODULE,PARAMS):
                 OUTDATA = PREFIX + INDATA
 
     elif MODULE.lower() == "epi_fm_realign_unwarp":
-        OUTSTR = ""
-        OUTDATA = ""
+        OUTSTR = 'fieldmap_correct.sh -f {}'.format(INDATA)
+        OUTDATA = 'u' + INDATA
+
+        if PARAMS:
+            if 'fm_name' in PARAMS:
+                FMSERIES = PARAMS['fm_name']
+                FMDCMDIR = os.path.abspath('../../fieldmaps/{}/dicom'.format(FMSERIES))
+                OUTSTR += ' -d {}'.format(FMDCMDIR)
+
+            if 'tert' in PARAMS:
+                TERT = PARAMS['tert']
+                OUTSTR += ' -t {}'.format(TERT)
+        
 
     elif MODULE.lower() == "retroicor_multiband":
         OUTSTR = 'multiband_retroicor.bash -f {}'.format(INDATA)
@@ -83,13 +94,19 @@ def make_script_call(INDATA,MODULE,PARAMS):
         OUTDATA = ""
 
     elif MODULE.lower() == "despike":
-        OUTSTR = ""
-        OUTDATA = ""
+        OUTSTR = 'despike.bash -f {} -r'.format(INDATA)
+        OUTDATA = 'f_' + INDATA
+
+    elif MODULE.lower() == 'cp_fieldmap_dcms':
+        OUTSTR = 'cp -pr {} dicom'.format(INDATA)
+        OUTDATA = INDATA + '/dicom'
+
+    elif MODULE.lower() == 'fieldmap_prep':
+        OUTSTR = 'fieldmap_prep.sh dicom/'
 
     else:
         OUTSTR='test'
 
-    print(OUTSTR)
     return OUTSTR, OUTDATA
 
 # Stub to get input of a module
@@ -117,7 +134,11 @@ def preproc(JSON):
 
             RAWDATA = SE_INFO['rawdata']
             MODULES = SE_INFO['module_order']
-            MODULE_PARAMS = SE_INFO['module_params']
+
+            if 'module_params' in SE_INFO.keys():
+                MODULE_PARAMS = SE_INFO['module_params']
+            else:
+                MODULE_PARAMS = {}
 
             print('\nStarting processing on {}...\n'.format(RAWDATA))
             print('Attempting the following modules:\n')
@@ -143,7 +164,13 @@ def preproc(JSON):
                     RAWDATA = os.path.abspath('{}/func/{}/{}/{}'.format(os.getcwd(),TASK,RUNNAME,RAWDATA))
                 elif SE_INFO['acqtype'] == 'epi':
                     RAWDATA = os.path.abspath('dicom/{}'.format(RAWDATA))
-					
+
+            elif SERIESTYPE.lower().startswith('fm_') and 'dti' not in SERIESTYPE.lower(): 
+                WORKINGDIR = '{}/func/fieldmaps/{}'.format(os.getcwd(),SERIESTYPE)
+
+            	# Absolute path of DCMDIR since we're changing into WORKINGDIR
+            	RAWDATA = os.path.abspath('dicom/{}'.format(RAWDATA))
+
             else:
                 WORKINGDIR = '{}/anatomy/{}'.format(os.getcwd(),SERIESTYPE)
 
@@ -170,9 +197,12 @@ def preproc(JSON):
 
 if __name__ == '__main__':
 
-    # anat.py executed as script
+    # func.py executed as script
     with open(argv[1]) as f:
         JSON = json.load(f)
+
+    #if 'func_fieldmap' in JSON.keys():
+    #    JSON = JSON['func_fieldmap']
 
     if 'func' in JSON.keys():
         JSON = JSON['func']
